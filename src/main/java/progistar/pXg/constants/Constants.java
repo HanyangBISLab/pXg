4de64dfca12ed95f98b1186daec2124e686d54f9
gistar <progistar@naver.com>
2025-08-04 22:25:50 +0900

    [feat] deal with Casanovo PTM annotation

--- src/main/java/progistar/pXg/constants/Constants.java
+++ src/main/java/progistar/pXg/constants/Constants.java
@@ -6,8 +6,8 @@ public class Constants {
 	//        plus, basically, this version number intends to upgrade behaviors of tool.
 	// minor: critical bug fix, change in results something like that.
 	// patch: tiny bug fix, typo, and change code styles something like that.
-	public static final String VERSION = "pXg v2.4.3";
-	public static final String RELEASE = "(release 2025-07-21)";
+	public static final String VERSION = "pXg v2.4.4";
+	public static final String RELEASE = "(release 2025-08-04)";
 	public static final String INTRODUCE = "Seunghyuk Choi and Eunok Paek in the Department of Computer Science at Hanyang University in Seoul, South Korea. ";
 	public static final String PMID = "38403075";
 
